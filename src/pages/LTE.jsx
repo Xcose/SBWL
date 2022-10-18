c4df883f0520a4c2b472b01bfe25ec6e08fc4cd7
Xcose <mfecanexhanti@gmail.com>
2022-10-18 11:16:48 +0200

    lte layout fix

--- src/pages/LTE.jsx
+++ src/pages/LTE.jsx
@@ -8,44 +8,74 @@ import blob2 from "../images/blob(5).svg";
 const LTE = () => {
 	return (
 		<div
-			className="lte-section d-flex align-items-stretch flex-row"
+			className="lte-section d-flex align-items-stretch flex-row row"
 			style={{ minHeight: "100vh" }}
 		>
 			{/* navigation bar */}
 			<SiteNav />
-			<div className="w-50 div1 position-relative">
-				<div className="position-relative top-50 start-50 translate-middle text-center h-100">
-					<img
-						src={blob1}
-						alt="..."
-						className="position-absolute top-50 start-50 translate-middle w-50"
-					/>
-					<img
-						src={blob}
-						alt="..."
-						className="position-absolute top-50 start-50 translate-middle w-50"
-					/>
-					<img
-						src={blob2}
-						alt="..."
-						className="position-absolute top-50 start-50 translate-middle w-50"
-					/>
-					<img
-						src={phone}
-						alt="..."
-						className="position-absolute top-50 start-50 translate-middle w-50"
-					/>
+			<div className="col-12 col-md-6 position-relative">
+				{/* Desktop */}
+				<div className="d-none d-md-block">
+					<div className="text-center h-100">
+						<img
+							src={blob1}
+							alt="..."
+							className="position-absolute top-50 start-50 translate-middle w-50"
+						/>
+						<img
+							src={blob}
+							alt="..."
+							className="position-absolute top-50 start-50 translate-middle w-50"
+						/>
+						<img
+							src={blob2}
+							alt="..."
+							className="position-absolute top-50 start-50 translate-middle w-50"
+						/>
+						<img
+							src={phone}
+							alt="..."
+							className="position-absolute top-50 start-50 translate-middle w-50"
+						/>
+					</div>
+					<div className="position-absolute top-50 start-50 translate-middle text-center text-white w-100">
+						<p className="display-1 text-center title">LTE</p>
+						<p className="display-6 text-center title">
+							Connect anywhere at any time
+						</p>
+					</div>
 				</div>
-				<div className="position-absolute top-50 start-50 translate-middle text-center text-white w-100">
-					<p className="display-1 text-center title">LTE</p>
-					<p className="display-6 text-center title">
-						Connect anywhere at any time
-					</p>
+				{/* mobile */}
+				<div className="d-block d-md-none">
+					<div className="text-center h-100">
+						<img src={blob1} alt="..." className="w-50" />
+						<img
+							src={blob}
+							alt="..."
+							className="position-absolute top-50 start-50 translate-middle w-50"
+						/>
+						<img
+							src={blob2}
+							alt="..."
+							className="position-absolute top-50 start-50 translate-middle w-50"
+						/>
+						<img
+							src={phone}
+							alt="..."
+							className="position-absolute top-50 start-50 translate-middle w-50"
+						/>
+					</div>
+					<div className="position-absolute top-50 start-50 translate-middle text-center text-white w-100">
+						<p className="display-1 text-center title">LTE</p>
+						<p className="display-6 text-center title">
+							Connect anywhere at any time
+						</p>
+					</div>
 				</div>
 			</div>
-			<div className="w-50">
+			<div className="col-12 col-md-6">
 				<div className="row position-relative top-50 start-50 translate-middle">
-					<div className="col-12 col-md-6">
+					<div className="col-12 col-md-6 py-2">
 						<div className="bg-lte-text rounded-2 p-2 h-100">
 							<p className="sub-title">Month-to-Month (No Contract)</p>
 							<p>
@@ -54,7 +84,7 @@ const LTE = () => {
 							</p>
 						</div>
 					</div>
-					<div className="col-12 col-md-6">
+					<div className="col-12 col-md-6 py-2">
 						<div className="bg-lte-text rounded-2 p-2 h-100">
 							<p className="sub-title">One month role over</p>
 							<p>
@@ -63,7 +93,7 @@ const LTE = () => {
 							</p>
 						</div>
 					</div>
-					<div className="col-12 col-md-6">
+					<div className="col-12 col-md-6 py-2">
 						<div className="bg-lte-text rounded-2 p-2 h-100">
 							<p className="sub-title">No out-of-bundle data</p>
 							<p>
@@ -72,7 +102,7 @@ const LTE = () => {
 							</p>
 						</div>
 					</div>
-					<div className="col-12 col-md-6">
+					<div className="col-12 col-md-6 py-2">
 						<div className="bg-lte-text rounded-2 p-2 h-100">
 							<p className="sub-title">
 								LTE ready, just insert simcard into device and you are ready
